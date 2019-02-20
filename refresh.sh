d51d8c2d782ac7c920e085c949a953bc0caa08fa
MTAPcoin <46968292+MTAPcoin@users.noreply.github.com>
2019-02-20 12:28:48 +0200

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -4,9 +4,9 @@ cd ~
 cd /usr/local/bin
 ./mtap-cli stop
 rm -rf mtapd mtap-cli mtap-tx
-wget https://github.com/MTAPcoin/MTAPCoin/releases/download/1.1.0/mtap-1.1.0-x86_64-linux-gnu.tar.gz
-tar -xzf mtap-1.1.0-x86_64-linux-gnu.tar.gz
-rm -rf mtap-1.1.0-x86_64-linux-gnu.tar.gz
+wget https://github.com/MTAPcoin/MTAPCoin/releases/download/1.2.0/mtap-1.2.0-x86_64-linux-gnu.tar.gz
+tar -xzf mtap-1.2.0-x86_64-linux-gnu.tar.gz
+rm -rf mtap-1.2.0-x86_64-linux-gnu.tar.gz
 ./mtapd -daemon
 sleep 10
 ./mtap-cli getinfo
