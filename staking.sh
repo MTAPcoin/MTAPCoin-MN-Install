6956ed2de4fd2b719bf3fa0b631d73166e37230e
MTAPcoin <46968292+MTAPcoin@users.noreply.github.com>
2019-04-13 15:04:48 +0200

    Update staking.sh

--- staking.sh
+++ staking.sh
@@ -28,9 +28,9 @@ sudo free
 sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
 cd
 
-wget https://github.com/MTAPcoin/MTAP/releases/download/2.0.0/mtap-2.0.0-x86_64-linux-gnu.tar.gz
-tar -xzf mtap-2.0.0-x86_64-linux-gnu.tar.gz
-rm -rf mtap-2.0.0-x86_64-linux-gnu.tar.gz
+wget https://github.com/MTAPcoin/MTAP/releases/download/2.1.0/mtap-2.1.0-x86_64-linux-gnu.tar.gz
+tar -xzf mtap-2.1.0-x86_64-linux-gnu.tar.gz
+rm -rf mtap-2.1.0-x86_64-linux-gnu.tar.gz
 
 sudo apt-get install -y ufw
 sudo ufw allow ssh/tcp
